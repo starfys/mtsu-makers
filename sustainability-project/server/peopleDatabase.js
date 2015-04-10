f854f3f690a6d069c8dff250349cbfc3baad5c32
Riuchando <sdkinser456@gmail.com>
2015-04-10 09:41:36 -0500

    with bootstrap

--- sustainability-project/server/peopleDatabase.js
+++ sustainability-project/server/peopleDatabase.js
@@ -1,5 +1,16 @@
 <script type="text/javascript">
+
+
+
+	//essentially the about page, might link to a more specific 'about' page
+	var people = [
+			{"firstName": "Stephen", "lastName":"Kinser", "github":"https://github.com/Riuchando"},
+			{"firstName": "Kris", "lastName":"Brown", "github":"https://github.com/KrisB88"},
+			{"firstName": "Steven", "lastName":"Sheffey", "github":"https://github.com/Starfys"}
+			];
+	
 function peopletable(){
+	
 	//yay I can Javascript
 	//essentially the about page, might link to a more specific 'about' page
 	var people = [
